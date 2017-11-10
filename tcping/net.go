067e80000c7fa20f0b7b34f846f7ac36e9fb3585
Shepherd Kendall <s@kndl.org>
2017-11-10 06:25:27 -0500

    formatting

--- tcping/net.go
+++ tcping/net.go
@@ -203,8 +203,8 @@ func to4byte(addr string) [4]byte {
 func printTCP(tcp *TCPHeader) {
 	var str string
 	str = fmt.Sprintf("[ SRC: %5d ] [ DST: %5d ]\n", tcp.Src, tcp.Dst)
-	str = str + fmt.Sprintf("[ SEQ: %10d ]\n", tcp.Seq)
-	str = str + fmt.Sprintf("[ ACK: %10d ]\n", tcp.Ack)
+	str = str + fmt.Sprintf("[ SEQ: %18d ]\n", tcp.Seq)
+	str = str + fmt.Sprintf("[ ACK: %18d ]\n", tcp.Ack)
 	str = str + fmt.Sprintf("[ ")
 	if tcp.HasFlag(URG) {
 		str = str + fmt.Sprintf("URG ")
